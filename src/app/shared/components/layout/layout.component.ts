9b1041d1d34259b95d3c358a55d81623d7f87e7d
Carlos Marrugo <elrprogramadortutoriales@gmail.com>
2024-10-23 11:54:28 -0500

    solve imports bugs from routes.ts

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutLet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
-import { SlidebarComponent } from '../slidebar/slidebar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, SlidebarComponent, FooterComponent, RouterOutLet],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrls: ['./layout.component.css']
 })
 export default class LayoutComponent {
-
+  
 }
