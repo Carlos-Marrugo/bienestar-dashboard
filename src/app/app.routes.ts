95412def0c82830cf308a91d0e8233b0e4aebc9c
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-10-25 01:21:42 -0500

    feat: add AuthService for handling user authentication and token management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -34,6 +34,10 @@ export const routes: Routes = [
     ],
   },
   {
+    path: 'login',
+    loadComponent: () => import('./business/authentication/login/login.component')
+  },
+  {
     path: '**',
     redirectTo: 'dashboard'
   }
