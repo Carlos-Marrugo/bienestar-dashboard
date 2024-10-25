114d8914611bd6eff0a04b992c2b2eddce02304e
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-10-25 00:34:23 -0500

    Added login component and fix dashboard navegation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,40 @@
 import { Routes } from '@angular/router';
 //routing
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./business/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent
+          ),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./business/profile/profile.component').then(
+            (m) => m.ProfileComponent
+          ),
+      },
+      {
+        path: 'tables',
+        loadComponent: () =>
+          import('./business/tables/tables.component').then(
+            (m) => m.TablesComponent
+          ),
+      },
+      {
         path: '',
-        loadComponent: () => import('./shared/components/layout/layout.component'),
-        children: [
-            {
-                path: 'dashboard',
-                loadComponent: () => import('./business/dashboard/dashboard.component').then(m => m.DashboardComponent),
-            },
-            {
-                path: 'profile',
-                loadComponent: () => import('./business/profile/profile.component').then(m => m.ProfileComponent)
-            },
-            {
-                path: 'tables',
-                loadComponent: () => import('./business/tables/tables.component').then(m => m.TablesComponent)
-            }
-        ]
-    }
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
 ];
